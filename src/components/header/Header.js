8f3372bbf4f0ecaa0092d169d05b62c809d78d04
Mattia Cintura <mattiacintura@icloud.com>
2023-05-25 20:35:03 +0200

    FIX: build error

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,11 +4,11 @@ import './Header.css';
 function Header() {
     return (
         <nav className="navbar border-bottom">
-            <a className="navbar-brand me-0">
+            <a className="navbar-brand me-0" href="javascript:void(0)">
                 <img src={logo} alt="Logo Loopable" className="Header-logo" />
             </a>
 
-            <a className="py-2 px-3 d-none d-sm-inline Primary-button">
+            <a className="py-2 px-3 d-none d-sm-inline Primary-button" href="javascript:void(0)">
                 Notify me
             </a>
         </nav>
