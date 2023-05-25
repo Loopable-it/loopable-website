54199937e241aab6446ab03676d7c8892f082fc4
Mattia Cintura <mattiacintura@icloud.com>
2023-05-25 19:49:44 +0200

    WIP: Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './assets/logo.svg';
+import logo from './assets/logo.svg'
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo"></img>
-      <p>Coming soon...</p>
-    </div>
+    <>
+      <Header></Header>
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo"></img>
+        <p>Coming soon...</p>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import logo from '../../assets/logo.svg';
+import './Header.css';
+
+function Header() {
+    return (
+        <nav className="navbar border-bottom">
+            <a className="navbar-brand me-0" href="#">
+                <img src={logo} alt="Logo Loopable" className="Header-logo" />
+            </a>
+
+            <a className="py-2 px-3 d-none d-sm-inline Primary-button" href="#">
+                Notify me
+            </a>
+        </nav>
+    );
+}
+
+export default Header;
