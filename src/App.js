2dfb4d490b240fe199479ce7d7e617fcd478a16c
Mattia Cintura <mattiacintura@icloud.com>
2023-05-28 19:40:52 +0200

    FIX: build error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import Features from './components/Features/Features';
-import './App.css';
 
 function App() {
   return (
