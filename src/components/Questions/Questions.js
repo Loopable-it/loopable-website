ebb98051ddb804fddd14cffc547a2bd9375f38ce
Mattia Cintura <mattiacintura@icloud.com>
2023-05-30 14:28:05 +0200

    FIX: Questions component mobile

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -5,31 +5,31 @@ import "./Questions.css";
 
 function Questions() {
     return (
-        <div className="Questions w-100 my-5">
-            <div className="row mx-0 d-flex align-items-center">
-                <div className="col-6">
+        <div className="Questions w-100 my-4">
+            <div className="Questions-card row mx-0 d-flex align-items-center mb-4">
+                <div className="col-12 col-sm-6 text-center text-sm-start">
                     <h2>What is Loopable?</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
                 </div>
-                <div className="col-6 d-flex justify-content-end">
+                <div className="d-flex col-12 order-first justify-content-center col-sm-6 order-sm-last justify-content-sm-end">
                       <img src={whatIs} alt="" />
                 </div>
             </div>
-            <div className="row mx-0 d-flex align-items-center">
-                <div className="col-6 d-flex justify-content-start">
+            <div className="Questions-card row mx-0 d-flex align-items-center mb-4">
+                <div className="d-flex col-12 justify-content-center col-sm-6 justify-content-sm-start">
                       <img src={whyShould} alt="" />
                 </div>
-                <div className="col-6">
+                <div className="col-12 col-sm-6 text-center text-sm-start">
                     <h2>Why should you use Loopable?</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
                 </div>
             </div>
-            <div className="row mx-0 d-flex align-items-center">
-                <div className="col-6">
+            <div className="Questions-card row mx-0 d-flex align-items-center mb-4">
+                <div className="col-12 col-sm-6 text-center text-sm-start">
                     <h2>How do we help our planet?</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
                 </div>
-                <div className="col-6 d-flex justify-content-end">
+                <div className="d-flex col-12 order-first justify-content-center col-sm-6 order-sm-last justify-content-sm-end">
                       <img src={how} alt="" />
                 </div>
             </div>
