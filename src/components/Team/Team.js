df75513022ed12d2ef3e4df36aefb52ad47f32ed
Mattia Cintura <mattiacintura@icloud.com>
2023-05-30 17:50:16 +0200

    FIX: build error

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -9,7 +9,7 @@ function Team() {
                     <div className="col-12 col-sm-6 text-center text-sm-start">
                         <h2>Our team</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
-                        <a className="Linkedin-link" href="https://www.linkedin.com/company/loopable-it" target="_blank">
+                        <a className="Linkedin-link" href="https://www.linkedin.com/company/loopable-it" target="_blank" rel="noreferrer">
                             <span>
                                 <i class="fa-brands fa-linkedin me-2"></i>
                                 Follow ou LikedIn page
