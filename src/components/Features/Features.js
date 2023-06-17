b11f63b5d1cac887f3435486e40173ca9f5d7d66
Mattia Cintura <mattiacintura@icloud.com>
2023-06-17 12:18:00 +0200

    REFACTOR: Features component

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -4,24 +4,37 @@ import "./Features.css";
 
 function Features(props) {
     const { t } = props;
+    const featuresList = [
+        {
+            icon: "fa-solid fa-hand-holding-dollar",
+            color: "#FB6107",
+            key: "Earn"
+        },
+        {
+            icon: "fa-solid fa-wallet",
+            color: "#0EB1D2",
+            key: "Rent"
+        },
+        {
+            icon: "fa-solid fa-leaf",
+            color: "#18CE40",
+            key: "Share"
+        }
+    ];
 
     return (
         <div className="Features w-100 row mx-0 text-center d-flex align-items-center">
-            <div className="col-12 col-sm-4">
-                <i class="fa-solid fa-hand-holding-dollar mb-3" style={{color: "#FB6107"}}/>
-                <h3>{t("Features_Earn")}</h3>
-                <p>{t("Features_Earn_Description")}</p>
-            </div>
-            <div className="col-12 col-sm-4">
-                <i class="fa-solid fa-wallet mb-3" style={{color: "#0EB1D2"}}/>
-                <h3>{t("Features_Rent")}</h3>
-                <p>{t("Features_Rent_Description")}</p>
-            </div>
-            <div className="col-12 col-sm-4">
-                <i class="fa-solid fa-leaf mb-3" style={{color: "#18CE40"}}/>
-                <h3>{t("Features_Share")}</h3>
-                <p>{t("Features_Share_Description")}</p>
-            </div>
+            {
+                featuresList.map((item) => {
+                    return (
+                        <div className="col-12 col-sm-4">
+                            <i className={`${item.icon} mb-3`} style={{color: item.color}}/>
+                            <h3>{t(`Features_${item.key}`)}</h3>
+                            <p>{t(`Features_${item.key}_Description`)}</p>
+                        </div>
+                    );
+                })
+            }
         </div>
     );
 }
