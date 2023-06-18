2690b086094f103155149789e37d2fb29a31fdf4
Mattia Cintura <mattiacintura@icloud.com>
2023-06-18 11:13:57 +0200

    FIX: Business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -24,13 +24,13 @@ function Business(props) {
         <div className="Business w-100 mb-0 text-center">
             <img src={logoBusiness} alt="Logo Loopable business" className="Business-logo"/>
 
-            <p className="w-75 my-4 mx-auto">{t("Business_Description")}</p>
+            <p className="Business-description my-4 mx-auto">{t("Business_Description")}</p>
 
-            <div className="row mx-0 d-flex justify-content-around">
+            <div className="row mx-0 mt-5 d-flex justify-content-around">
                 {
                     cardContent.map((item) => {
                         return (
-                            <div className="Business-card p-3 mb-3 mb-sm-0 col-12 col-sm-4">
+                            <div className="Business-card p-4 mb-3 mb-sm-0 col-12 col-sm-4">
                                 <i className={item.icon}></i>
                                 <h5 className="mt-3">{t(`Business_${item.key}`)}</h5>
                                 <p className="mx-sm-5">{t(`Business_${item.key}_Description`)}</p>
