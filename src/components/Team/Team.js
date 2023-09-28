f022ad3e0e90365fe07e2df194f5747197c5d58d
Mattia Cintura <mattiacintura@icloud.com>
2023-09-28 13:18:20 +0200

    FIX: Role description

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -5,56 +5,55 @@ import MattiaPic from "../../assets/photo_mattia.png";
 import "./Team.css";
 
 function Team(props) {
-    const { t } = props;
-    const teamList = [
-        {
-            image: MaggyPic,
-            name: "Maggy Protasio",
-            role: "Co-Founder · Business Lead",
-            description: "Team_Member_Maggy"
-        },
-        {
-            image: MattiaPic,
-            name: "Mattia Cintura",
-            role: "Co-Founder · Tech Lead",
-            description: "Team_Member_Mattia"
-        },
-    ];
+  const { t } = props;
+  const teamList = [
+    {
+      image: MaggyPic,
+      name: "Maggy Protasio",
+      role: "Co-Founder · Project Lead",
+      description: "Team_Member_Maggy",
+    },
+    {
+      image: MattiaPic,
+      name: "Mattia Cintura",
+      role: "Co-Founder · Tech Lead",
+      description: "Team_Member_Mattia",
+    },
+  ];
 
-    return (
-        <div className="Team w-100 mb-4">
-            <div className="Team-card">
-                <div className="Team-card-content row mx-0 text-center d-flex align-items-center px-0 px-xl-5">
-                    <div className="col-12 pb-5">
-                        <h2>{t("Team_OurTeam")}</h2>
-                        <p>{t("Team_OurTeam_Description")}</p>
-                        <a className="Linkedin-link" href="https://www.linkedin.com/company/loopable-it" target="_blank" rel="noreferrer">
-                            <span>
-                                <i className="fa-brands fa-linkedin me-2"></i>
-                                {t("Team_FollowLinkedIn")}
-                            </span>
-                        </a>
-                    </div>
-                    {
-                        teamList.map((item) => {
-                            return (
-                                <div className="Team-member col-12 col-xl-6 mb-3 mb-xl-0">
-                                    <img src={item.image} alt={item.name} className="mb-2" />
-                                    <h3 className="mb-2">{item.name}</h3>
-                                    <p className="text-secondary mb-1">
-                                        {item.role}
-                                    </p>
-                                    <h6>
-                                        {t(item.description)}
-                                    </h6>
-                                </div>
-                            );
-                        })
-                    }
-                </div>
-            </div>
+  return (
+    <div className="Team w-100 mb-4">
+      <div className="Team-card">
+        <div className="Team-card-content row mx-0 text-center d-flex align-items-center px-0 px-xl-5">
+          <div className="col-12 pb-5">
+            <h2>{t("Team_OurTeam")}</h2>
+            <p>{t("Team_OurTeam_Description")}</p>
+            <a
+              className="Linkedin-link"
+              href="https://www.linkedin.com/company/loopable-it"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>
+                <i className="fa-brands fa-linkedin me-2"></i>
+                {t("Team_FollowLinkedIn")}
+              </span>
+            </a>
+          </div>
+          {teamList.map((item) => {
+            return (
+              <div className="Team-member col-12 col-xl-6 mb-3 mb-xl-0">
+                <img src={item.image} alt={item.name} className="mb-2" />
+                <h3 className="mb-2">{item.name}</h3>
+                <p className="text-secondary mb-1">{item.role}</p>
+                <h6>{t(item.description)}</h6>
+              </div>
+            );
+          })}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default withTranslation()(Team);
