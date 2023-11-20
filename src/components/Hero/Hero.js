0306b09240c0d1186955cd3b30c5c462a1ef2f98
Mattia Cintura <mattiacintura@icloud.com>
2023-11-20 21:45:36 +0100

    FIX: Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,26 +10,24 @@ function Hero(props) {
     return (
         <div className="Hero w-100">
             <div className="Hero-container row mx-0">
-                <div className="col-12 col-md-6 px-0 align-self-center">
+                <div className="col-12 col-md-6 px-0 align-self-center mt-5 mt-sm-0">
                     <span className="small-title bg-white rounded-3 py-2 px-3">
                         <i class="fa-solid fa-star me-2" style={{color: '#18ce40'}}></i>
-                        The best way to rent items
+                        {t("Hero_Badge")}
                     </span>
 
                     <h1 className="extra-large-title my-4">
-                        Renting items <br/>
-                        <span className="green-text">made easy</span>
+                        {t("Hero_Title_1")}<br/>
+                        <span className="green-text">{t("Hero_Title_2")}</span>
                     </h1>
 
                     <p className="medium-text grey-text p-0">
-                        Loopable revolutionizes the concept of renting by providing an innovative platform that allows users to find the items they need, whenever and wherever they want.
+                        {t("Hero_Description")}
                     </p>
                 </div>
 
                 <div className="col-12 col-md-6 p-0 align-self-end">
-                    <div>
                     <img src={mockup} className="img-fluid m-0"/>
-                    </div>
                 </div>
             </div>
         </div>
