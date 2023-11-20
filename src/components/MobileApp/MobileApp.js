4bf8601bd4696f1835a55d787461a717b4d533a5
Mattia Cintura <mattiacintura@icloud.com>
2023-11-20 23:49:54 +0100

    FIX: Mobile app section

--- src/components/MobileApp/MobileApp.js
+++ src/components/MobileApp/MobileApp.js
@@ -7,28 +7,28 @@ function MobileApp(props) {
 
   return (
     <div className="Features w-100 row mx-0">
-      <div className="col mb-5">
+      <div className="col-12 col-sm mb-5">
           <p className="small-bold-text white-text p-0">
-              <span className="green-text">-</span> Download the Loopable app
+              <span className="green-text">-</span> {t("Mobile_App_Section_Title")}
           </p>
 
           <h2 className="large-title white-text">
-            Download our<br/>
-              <span className="green-text">mobile app</span>
+            {t("Mobile_App_Title_1")}<br/>
+              <span className="green-text">{t("Mobile_App_Title_2")}</span>
           </h2>
 
           <p className="medium-text white-text pe-5">
-            The app is not availbale yet, subscribe to our newsletter to be notified when it will become availbale.
+            {t("Mobile_Not_Available")}
           </p>
 
           <button
-            className="green-button d-none d-sm-inline"
+            className="green-button"
           >
             Subscribe
           </button>
       </div>
 
-      <div className="col mb-5">
+      <div className="col-12 col-sm mb-5">
         <div className="bg-white bg-opacity-50 rounded-4 p-4">
           <i className="fa-brands fa-apple mb-5" style={{fontSize: "30px"}}></i>
           <p className="medium-bold-text white-text mt-5">Loopable on iOS</p>
@@ -41,7 +41,7 @@ function MobileApp(props) {
           </button>
         </div>
       </div>
-      <div className="col mb-5">
+      <div className="col-12 col-sm mb-5">
         <div className="bg-white bg-opacity-50 rounded-4 p-4">
           <i className="fa-brands fa-android mb-5" style={{fontSize: "30px"}}></i>
           <p className="medium-bold-text white-text mt-5">Loopable on Android</p>
